fc4a428bce7f9a2c55cab77ab6330f39fbe58ebc
fy1302uc <121342443+fy1302uc@users.noreply.github.com>
2023-01-01 07:44:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 (function(){
   var h6=document.querySelector('.wrap>header>h6')
   var time=new Date();
-  h6.innerHTML='※今天是 '+time.getFullYear()+'年'+time.getMonth()+'月'+time.getDate()+'日'+' 好象已经过了哦！※'
+  h6.innerHTML='※今天是 '+time.getFullYear()+'年'+(time.getMonth()+1)+'月'+time.getDate()+'日'+' 好象已经过了哦！※'
   
 var count = 0;
     var maxTop = document.body.scrollHeight - innerHeight;
